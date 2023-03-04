b9ad8bebb6816b39e7253fc33bc2efd19fccc3ce
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2023-03-04 12:40:21 +0530

    line-class-cleaned

--- src/Line.ts
+++ src/Line.ts
@@ -4,11 +4,13 @@ export class Line {
 	private _p1: Point;
 	private _p2: Point;
 	private _slope: number;
+	private _yIntercept: number;
 
 	public constructor(p1: Point, p2: Point) {
 		this.p1 = p1;
 		this.p2 = p2;
 		this.slope = this.p1.getSlope(this.p2);
+		this.yIntercept = this.calculateYIntercept();
 	}
 
 	public get p1(): Point {
@@ -35,11 +37,31 @@ export class Line {
 		this._slope = value;
 	}
 
+	public get yIntercept(): number {
+		return this._yIntercept;
+	}
+	public set yIntercept(value: number) {
+		this._yIntercept = value;
+	}
+
 	public getPerpendicularSlope(): number {
 		return -1 * (1 / this.slope);
 	}
 
+	public calculateYIntercept(): number {
+		const p = this.p1;
+		return p.y - this.slope * p.x;
+	}
+
+	public getLineEquation(): string {
+		return `y = ${this.slope}x + ${this.yIntercept}`;
+	}
+
+	public isPointOnLine(p: Point): boolean {
+		return p.y == this.slope * p.x + this.yIntercept;
+	}
+
 	public toString(): string {
-		return `Line: (${this.p1.toString()}, ${this.p2.toString()})`;
+		return `Line: (${this.p1.toString()}, ${this.p2.toString()})\nLineEquation: ${this.getLineEquation()}`;
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,10 @@
 import { Line } from "./Line";
-import { LineEquation } from "./LineEquation";
 import { Point } from "./Point";
 const main = () => {
-	const p1 = new Point(1, 2);
-	const p2 = new Point(-1, 0);
-	// console.log(
-	// 	`distance from origin to ${p1.toString()} is ${p2.getDistance()}`
-	// );
-	// console.log(
-	// 	`distance from ${p1.toString()} to ${p2.toString()} is ${p1.getDistance(
-	// 		p2
-	// 	)}`
-	// );
-	// console.log(
-	// 	`slope of ${p1.toString()} and ${p2.toString()} is ${p1.getSlope(p2)}`
-	// );
-	const l1 = new Line(p1, p2);
-	const le = new LineEquation(l1);
-	console.log(l1.toString());
-	console.log(le.toString());
-	const origin = new Point(0, 0);
-	const p3 = new Point(5, 6);
-	console.log(le.isPointOnLine(origin));
-	console.log(le.isPointOnLine(p3));
+	const p1 = new Point(1, 1);
+	const p2 = new Point(2, 2);
+	const l = new Line(p1, p2);
+	console.log(l.toString());
 };
 
 main();
